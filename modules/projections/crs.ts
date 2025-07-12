a609b772a60769007c71b899d2d1fabd4770c6f0
Felix Palmer <felixpalmer@gmail.com>
2025-07-12 20:33:24 +0200

    CRS.getVertex

--- modules/projections/crs.ts
+++ modules/projections/crs.ts
@@ -5,6 +5,16 @@ import type { Cartesian, Radians, Spherical } from "../core/coordinate-systems";
 import { toCartesian } from "../core/coordinate-transforms";
 import { origins } from "../core/origin";
 
+/**
+ * The Coordinate Reference System (CRS) of the dodecahedron is a set of 62 vertices:
+ * - 12 face centers
+ * - 20 vertices
+ * - 30 edge midpoints
+ * 
+ * The vertices are used as a rigid frame of reference for the dodecahedron in the
+ * dodecahedron projection. By constructing them once, we can avoid recalculating
+ * and be sure of their correctness.
+ */
 export class CRS {
   private vertices: Cartesian[] = [];
 
@@ -17,6 +27,16 @@ export class CRS {
     Object.freeze(this.vertices);
   }
 
+  getVertex(point: Cartesian): Cartesian {
+    for (const vertex of this.vertices) {
+      if (vec3.distance(point, vertex) < 1e-5) {
+        return vertex;
+      }
+    }
+
+    throw new Error("Failed to find vertex in CRS");
+  }
+
   private addFaceCenters(): void {
     origins.forEach(origin => this.add(toCartesian(origin.axis)));
   }
@@ -27,13 +47,11 @@ export class CRS {
 
     for (const origin of origins) {
       for (let i = 0; i < 5; i++) {
-        // Midpoint
         const thetaMidpoint = (2 * i) * Math.PI / 5 as Radians;
         const midpoint = toCartesian([thetaMidpoint + origin.angle, phiMidpoint] as Spherical);
         vec3.transformQuat(midpoint, midpoint, origin.quat);
         this.add(midpoint);
 
-        // Vertex
         const thetaVertex = (2 * i + 1) * Math.PI / 5 as Radians;
         const vertex = toCartesian([thetaVertex + origin.angle, phiVertex] as Spherical);
         vec3.transformQuat(vertex, vertex, origin.quat);
