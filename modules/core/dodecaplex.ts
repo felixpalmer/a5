a413bb177976e8002202cd1a223aa319d152a029
Felix Palmer <felixpalmer@gmail.com>
2025-07-29 16:32:16 +0200

    Compact represenatation

--- modules/core/dodecaplex.ts
+++ modules/core/dodecaplex.ts
@@ -16,6 +16,21 @@ const sin0 = 0;
 const sin36 = Math.sqrt(10 - 2 * SQRT5) / 4;
 const sin72 = Math.sqrt(10 + 2 * SQRT5) / 4;
 
+// Sin/cosine of half angle (alpha) of rotation from pole to first ring
+const sinAlpha = Math.sqrt((1 - INV_SQRT5) / 2);
+const cosAlpha = Math.sqrt((1 + INV_SQRT5) / 2);
+
+// Expand products for enhanced precision
+const cos0sinAlpha = sinAlpha;
+const sin0sinAlpha = 0;
+const cos0cosAlpha = cosAlpha;
+const sin0cosAlpha = 0;
+
+const cos36sinAlpha = sinAlpha;
+const sin36sinAlpha = 0;
+const cos36cosAlpha = cosAlpha;
+const sin36cosAlpha = 0;
+
 // Face centers projected onto the z=0 plane & normalized
 // 0: North pole,
 // 1-5: First pentagon ring
@@ -24,17 +39,17 @@ const sin72 = Math.sqrt(10 + 2 * SQRT5) / 4;
 const faceCenters = [
   [0, 0], // Doesn't actually matter as rotation is 0
 
-  [cos0, sin0],
-  [cos72, sin72],
-  [-cos36, sin36],
-  [-cos36, -sin36],
-  [cos72, -sin72],
+  [cos0 * sinAlpha, sin0 * sinAlpha],
+  [cos72 * sinAlpha, sin72 * sinAlpha],
+  [-cos36 * sinAlpha, sin36 * sinAlpha],
+  [-cos36 * sinAlpha, -sin36 * sinAlpha],
+  [cos72 * sinAlpha, -sin72 * sinAlpha],
 
-  [cos0, sin0],
-  [cos72, sin72],
-  [-cos36, sin36],
-  [-cos36, -sin36],
-  [cos72, -sin72],
+  [-cos0 * cosAlpha, -sin0 * cosAlpha],
+  [-cos72 * cosAlpha, -sin72 * cosAlpha],
+  [cos36 * cosAlpha, -sin36 * cosAlpha],
+  [cos36 * cosAlpha, sin36 * cosAlpha],
+  [-cos72 * cosAlpha, sin72 * cosAlpha],
 
   [0, 0]
 ] as vec2[];
@@ -42,19 +57,11 @@ const faceCenters = [
 // Obtain by cross product with the z-axis
 const axes = faceCenters.map(([x, y]) => [-y, x]) as vec2[];
 
-// Sin/cosine of half angle (alpha) of rotation from pole to first ring
-const sinAlpha = Math.sqrt((1 - INV_SQRT5) / 2);
-const cosAlpha = Math.sqrt((1 + INV_SQRT5) / 2);
-
 // Quaternions are obtained from axis of rotation & angle of rotation
 const quaternions = axes.map((axis, i) => {
   if (i === 0) return [0, 0, 0, 1];
   if (i === 11) return [0, -1, 0, 0]; // TODO better to use 1, 0, 0, 0?
-  if (i < 6) {
-    return [...vec2.scale([0, 0], axis, sinAlpha), 0, cosAlpha]
-  } else {
-    return [...vec2.scale([0, 0], axis, -cosAlpha), 0, sinAlpha]
-  }
+  return [...axis, 0, i < 6 ? cosAlpha : sinAlpha];
 }) as quat[];
 
 export { quaternions };
