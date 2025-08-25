87287823395c948123f02f87876933409778dd8f
felixpalmer <felixpalmer@gmail.com>
2025-08-25 21:48:22 +0200

    Add more tests for constants & dodecahedron-quats (#53)

--- scripts/generate-fixtures.cjs
+++ scripts/generate-fixtures.cjs
@@ -5,6 +5,8 @@ require("./fixtures/core/cell-info.cjs");
 require("./fixtures/core/crs-vertices.cjs");
 require("./fixtures/core/origins.cjs");
 require("./fixtures/core/tiling.cjs");
+require("./fixtures/core/constants.cjs");
+require("./fixtures/core/dodecahedron-quaternions.cjs");
 
 // Geometry generators
 require("./fixtures/geometry/spherical-polygon.cjs");
