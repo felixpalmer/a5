87252e3e88d8ad49e7998224d4cf7d1ab1b60fff
Felix Palmer <felixpalmer@gmail.com>
2026-05-09 20:20:06 +0200

    Review text

--- website/src/examples/polygons.js
+++ website/src/examples/polygons.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import {GITHUB_TREE} from '../constants/defaults';
-import App from 'website-examples/polygons/app';
+import React, { Component } from "react";
+import { GITHUB_TREE } from "../constants/defaults";
+import App from "website-examples/polygons/app";
 
-import {makeExample} from '../components';
+import { makeExample } from "../components";
 
 class PolygonsDemo extends Component {
-  static title = 'Polygons';
+  static title = "Polygons";
 
   static code = `${GITHUB_TREE}/examples/website/polygons`;
 
@@ -14,14 +14,32 @@ class PolygonsDemo extends Component {
   static renderInfo(meta) {
     return (
       <div>
-        <p>Click on the map to place waypoints. Cells along the great-circle arcs between consecutive waypoints are traced and displayed.</p>
+        <p>Click on the map to place waypoints, or load a country preset.</p>
+        <p>
+          Depending on the mode,{" "}
+          <a href="/docs/api-reference/regions#polygontocells">
+            <code>polygonToCells</code>
+          </a>{" "}
+          is used to show the cells enclosed, or{" "}
+          <a href="/docs/api-reference/traversal#linestringtocells">
+            <code>lineStringToCells</code>
+          </a>{" "}
+          is used to show cells which overlap the outline
+        </p>
       </div>
     );
   }
 
   render() {
     return (
-      <div style={{width: '100%', height: '100%', position: 'absolute', background: '#111'}}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          background: "#111",
+        }}
+      >
         <App {...this.props} />
       </div>
     );
