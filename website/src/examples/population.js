419dd536e9167b7c14cd2520dceb7d93168149ca
Felix Palmer <felixpalmer@gmail.com>
2026-07-12 13:21:46 +0200

    more datasets

--- website/src/examples/population.js
+++ website/src/examples/population.js
@@ -19,11 +19,14 @@ class PopulationDemo extends Component {
           Queries run live in your browser using{' '}
           <a href="https://duckdb.org/docs/stable/clients/wasm/overview.html">DuckDB WASM</a> with the{' '}
           <a href="https://duckdb.org/community_extensions/extensions/a5">A5 community extension</a>. Edit the SQL to
-          re-aggregate the cells to a different resolution, or filter to the most densely populated areas.
+          re-aggregate the cells to a different resolution, filter to the most densely populated areas, or select a
+          country by name. The <code>elevation</code> and <code>temperature</code> tables share the same resolution-9
+          cells, so they can be joined against <code>population</code>.
         </p>
         <p>
           Data: <a href="https://data.humdata.org/dataset/kontur-population-dataset-3km">Kontur Population</a> (CC BY
-          4.0)
+          4.0), <a href="https://www.ncei.noaa.gov/products/etopo-global-relief-model">ETOPO 2022</a> (public domain),{' '}
+          <a href="https://worldclim.org/data/worldclim21.html">WorldClim 2.1</a>
         </p>
       </div>
     );
