66f5c2ce2195be87466f22d77a62a5568f709b99
Felix Palmer <felixpalmer@gmail.com>
2026-09-23 18:47:30 +0300

    tweak

--- website/src/examples/projection.js
+++ website/src/examples/projection.js
@@ -18,17 +18,17 @@ class ProjectionDemo extends Component {
           A5 uses the <a href="https://proj.org/en/stable/operations/projections/dsea.html#a5">DSEA</a> projection to map the planar coordinates from the dodecahedron face onto the sphere.
         </p>
         <p>
-          The Jacobian describes how this mapping behaves locally, and can be decomposed into more intuitive rotation, shear and scale matrices.
+          The Jacobian describes how this mapping behaves locally, and can be decomposed into more intuitive rotation, scale, shear and squash matrices.
         </p>
         <p>
-          While there are a number of other similar projections, DSEA was chosen as it produces minimal cusps (2°) and
+          While there are a number of other similar projections, DSEA was chosen as it produces
           minimal sag (deviation from great circles) in cell edges, compared to{' '}
-          <a href="https://proj.org/en/stable/operations/projections/isea.html#dual">ISEA</a> and RTSEA.
+          <a href="https://proj.org/en/stable/operations/projections/isea.html#dual">ISEA</a> and RTSEA. It has minimal cusps (2°) and while ISEA is better here both are practicially inperceptible.
         </p>
         <p>
           The remaining three{' '}
           <a href="https://www.tandfonline.com/doi/abs/10.1559/152304006779500687">Parallel Small Circle projections</a>{' '}
-          have similar metrics to DSEA but no closed form inverse so are more computationally intensive.
+          have similar metrics to DSEA but no known closed form inverse so are more computationally intensive.
         </p>
       </div>
     );
